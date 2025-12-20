d4dc2f61fc5e08b512a2582a9a44f32cbc10c95b
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-12-20 11:48:19 -0600

    Format code

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
-let names = document.getElementById('name');
-let quantity = document.getElementById('quantity');
-let price = document.getElementById('price');
-let tax = document.getElementById('tax');
-let orderList = document.getElementById('orders');
+let names = document.getElementById("name");
+let quantity = document.getElementById("quantity");
+let price = document.getElementById("price");
+let tax = document.getElementById("tax");
+let orderList = document.getElementById("orders");
 let orders = [];
 
 function addOrder() {
-    if (names.value && !isNaN(parseInt(quantity.value)) && !isNaN(parseFloat(price.value)) && !isNaN(parseFloat(tax.value))) {
-        orders.push({
-            names: names.value, quantity: parseInt(quantity.value), price: parseInt(price.value * 100) / 100, tax: parseInt(tax.value * 100) / 100
-        });
-        names.value = '';
-        quantity.value = '';
-        price.value = '';
-        tax.value = '';
-        updateList();
-    }
+  if (
+    names.value &&
+    !isNaN(parseInt(quantity.value)) &&
+    !isNaN(parseFloat(price.value)) &&
+    !isNaN(parseFloat(tax.value))
+  ) {
+    orders.push({
+      names: names.value,
+      quantity: parseInt(quantity.value),
+      price: parseInt(price.value * 100) / 100,
+      tax: parseInt(tax.value * 100) / 100,
+    });
+    names.value = "";
+    quantity.value = "";
+    price.value = "";
+    tax.value = "";
+    updateList();
+  }
 }
 function updateList() {
-    let total = orders.reduce((totalPrice, order) => totalPrice + order.quantity * order.price * (1 + order.tax / 100), 0);
+  let total = orders.reduce(
+    (totalPrice, order) =>
+      totalPrice + order.quantity * order.price * (1 + order.tax / 100),
+    0,
+  );
 }
