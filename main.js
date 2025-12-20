f6ea1cc4fa0c2b584fca4e40bd8d349b314c4436
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-12-20 11:48:36 -0600

    Add Prettier and format code

--- main.js
+++ main.js
@@ -6,29 +6,29 @@ let orderList = document.getElementById("orders");
 let orders = [];
 
 function addOrder() {
-  if (
-    names.value &&
-    !isNaN(parseInt(quantity.value)) &&
-    !isNaN(parseFloat(price.value)) &&
-    !isNaN(parseFloat(tax.value))
-  ) {
-    orders.push({
-      names: names.value,
-      quantity: parseInt(quantity.value),
-      price: parseInt(price.value * 100) / 100,
-      tax: parseInt(tax.value * 100) / 100,
-    });
-    names.value = "";
-    quantity.value = "";
-    price.value = "";
-    tax.value = "";
-    updateList();
-  }
+    if (
+        names.value &&
+        !isNaN(parseInt(quantity.value)) &&
+        !isNaN(parseFloat(price.value)) &&
+        !isNaN(parseFloat(tax.value))
+    ) {
+        orders.push({
+            names: names.value,
+            quantity: parseInt(quantity.value),
+            price: parseInt(price.value * 100) / 100,
+            tax: parseInt(tax.value * 100) / 100,
+        });
+        names.value = "";
+        quantity.value = "";
+        price.value = "";
+        tax.value = "";
+        updateList();
+    }
 }
 function updateList() {
-  let total = orders.reduce(
-    (totalPrice, order) =>
-      totalPrice + order.quantity * order.price * (1 + order.tax / 100),
-    0,
-  );
+    let total = orders.reduce(
+        (totalPrice, order) =>
+            totalPrice + order.quantity * order.price * (1 + order.tax / 100),
+        0,
+    );
 }
